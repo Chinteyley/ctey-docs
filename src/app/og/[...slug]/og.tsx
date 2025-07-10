338e5999e511a5d8ab94bd04a0882cca42435129
chintey <kimteyley@gmail.com>
2025-07-10 23:31:00 +0700

    Increase SVG size for improved visibility and clarity

--- src/app/og/[...slug]/og.tsx
+++ src/app/og/[...slug]/og.tsx
@@ -77,7 +77,7 @@ export function generate({
                         color: primaryTextColor,
                     }}
                 >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    <svg width="60" height="60" viewBox="0 0 180 180" fill="none"
                          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                          className="">
                         <path d="M10.5 16h3"></path>
