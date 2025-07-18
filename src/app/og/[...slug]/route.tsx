f757616f5c5586516054047ffca0d152dcd56bc1
chintey <kimteyley@gmail.com>
2025-07-18 11:30:57 +0700

    Add meta.json for project metadata and introduce Vuth's VVar game description

--- src/app/og/[...slug]/route.tsx
+++ src/app/og/[...slug]/route.tsx
@@ -3,25 +3,24 @@ import { source } from '@/lib/source';
 import { notFound } from 'next/navigation';
 
 export async function GET(
-    _req: Request,
-    { params }: { params: Promise<{ slug: string[] }> },
+	_req: Request,
+	{ params }: { params: Promise<{ slug: string[] }> },
 ) {
-    const { slug } = await params;
-    const page = source.getPage(slug.slice(0, -1));
-    if (!page) notFound();
-
-    return generateOGImage({
-        primaryTextColor: 'rgb(240,240,240)',
-        title: page.data.title,
-        description: page.data.description,
-    });
+	const { slug } = await params;
+	const page = source.getPage(slug.slice(0, -1));
+	if (!page) notFound();
+	return generateOGImage({
+		primaryTextColor: 'rgb(240,240,240)',
+		title: page.data.title,
+		description: page.data.description,
+	});
 }
 
 export function generateStaticParams(): {
-    slug: string[];
+	slug: string[];
 }[] {
-    return source.generateParams().map((page) => ({
-        ...page,
-        slug: [...page.slug, 'image.png'],
-    }));
+	return source.generateParams().map((page) => ({
+		...page,
+		slug: [...page.slug, 'image.png'],
+	}));
 }
