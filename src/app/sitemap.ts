1ceb9882c44113af23d204ffd37d9f5b7e7d0b65
chintey <96514533+Chinteyley@users.noreply.github.com>
2025-07-18 13:20:51 +0700

    Merge pull request #6
    * Improve SEO by updating metadata and JSON-LD structured data

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://ctey-docs.vercel.app'; // Replace with your actual domain
+  const baseUrl = 'https://ctey.dev'; // Replace with your actual domain
   
   // Define the current date for lastModified
   const currentDate = new Date();
